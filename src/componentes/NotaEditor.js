0ddac3614d38a6cb964b361ccc158ed252568b63
neves1697 <neves1697@gmail.com>
2024-02-03 17:18:07 -0300

    Refatorado a estilização, separado na pasta (Styles)

--- src/componentes/NotaEditor.js
+++ src/componentes/NotaEditor.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Modal, View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
+import estilos from "../../styles/NotaEditorStyle";
 
 //importanto async storage
 //import { useAsyncStorage } from "@react-native-async-storage/async-storage";
@@ -20,7 +21,7 @@ export default function NotaEditor() {
 
     console.log(umaNota);
     await AsyncStorageLib.setItem(umaNota.id, umaNota.texto);
-    
+
     mostraNota();
 
   }
@@ -80,104 +81,3 @@ export default function NotaEditor() {
   )
 }
 
-const estilos = StyleSheet.create({
-  centralizaModal: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "flex-end"
-  },
-  modal: {
-    backgroundColor: "#FFFFFF",
-    paddingHorizontal: 16,
-    paddingTop: 16,
-    paddingBottom: 32,
-    marginTop: 8,
-    marginHorizontal: 16,
-    borderTopLeftRadius: 8,
-    borderTopRightRadius: 8,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
-    shadowOpacity: 0.34,
-    shadowRadius: 6.27,
-    elevation: 10,
-  },
-  modalTitulo: {
-    fontSize: 28,
-    fontWeight: "600",
-    marginBottom: 18,
-  },
-  modalInput: {
-    fontSize: 18,
-    marginBottom: 12,
-    paddingHorizontal: 4,
-    borderBottomWidth: 1,
-    borderBottomColor: "#FF9A94",
-  },
-  modalPicker: {
-    borderWidth: 1,
-    borderRadius: 5,
-    borderColor: "#EEEEEE",
-    marginBottom: 12,
-  },
-  modalSubTitulo: {
-    fontSize: 14,
-    marginBottom: 8,
-    fontWeight: "600"
-  },
-  modalBotoes: {
-    flexDirection: "row",
-    justifyContent: "space-between"
-  },
-  modalBotaoSalvar: {
-    backgroundColor: "#2ea805",
-    borderRadius: 5,
-    padding: 8,
-    width: 80,
-    alignItems: "center",
-  },
-  modalBotaoDeletar: {
-    backgroundColor: "#d62a18",
-    borderRadius: 5,
-    padding: 8,
-    width: 80,
-    alignItems: "center",
-  },
-  modalBotaoCancelar: {
-    backgroundColor: "#057fa8",
-    borderRadius: 5,
-    padding: 8,
-    width: 80,
-    alignItems: "center",
-  },
-  modalBotaoTexto: {
-    color: "#FFFFFF",
-  },
-  adicionarMemo: {
-    backgroundColor: "#54ba32",
-    justifyContent: "center",
-    height: 64,
-    width: 64,
-    margin: 16,
-    alignItems: "center",
-    borderRadius: 9999,
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-    elevation: 4,
-  },
-  adicionarMemoTexto: {
-    fontSize: 32,
-    lineHeight: 40,
-    color: "#FFFFFF",
-  }
-});
